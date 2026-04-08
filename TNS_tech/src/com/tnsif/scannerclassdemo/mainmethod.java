0cec65cfb737f338736e2682ecc5f0ba105ef243
Tanishams <tanishams225@gmail.com>
2026-04-08 12:51:33 +0530

    Tax calculation done

---
+++ TNS_tech/src/com/tnsif/scannerclassdemo/mainmethod.java
@@ -0,0 +1,24 @@
+package com.tnsif.scannerclassdemo;
+import java.util.Scanner;
+public class mainmethod {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the person name");
+		String name=sc.next();
+		System.out.println("enter the income");
+		int income=sc.nextInt();
+		
+		Person pp=new Person();
+		pp.setName(name);
+		pp.setIncome(income);
+		
+		Taxcalculation t=new Taxcalculation();
+		t.CalculateTax(pp);
+		
+		System.out.println("after tax calculation");
+		
+		System.out.println(pp);
+	}
+
+	}
